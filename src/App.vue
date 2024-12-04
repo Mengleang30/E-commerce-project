0cfd5a7198e4632dc45b326fc05c198db5592831
Sonavin <sonavin.rem@gmail.com>
2024-12-04 13:10:10 +0700

    Install material for running the project
    change name a little bit
    npm install: pinia, router, ...

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,33 @@
 <script setup>
-
-import Card_book from './Layout/Card_book.vue';
-import Footer from './Layout/Footer.vue';
-import Header from './Layout/Header.vue';
-import { RouterView } from 'vue-router';
-
-
-
-
-
+import Card_book from "./Layout/Card_book.vue";
+import Footer from "./Layout/Footer.vue";
+import Header from "./Layout/Header.vue";
+import { RouterView } from "vue-router";
 </script>
 
 <template>
+  <Header />
+  <main class="Main_page">
+    <div class="wrap_card">
+      <Card_book />
+    </div>
+    <RouterView />
+  </main>
 
-    <Header />
-    <main class="Main_page">
-        <div class="wrap_card">
-            <Card_book />
-        </div>
-        <RouterView />
-    </main>
-
-    <Footer />
-
+  <Footer />
 </template>
 
 <style scoped>
 .Main_page {
-    min-height: 90vh;
-    padding: 2px;
+  min-height: 90vh;
+  padding: 2px;
 }
-.wrap_card{
-    display: flex;
-    margin-top: 0.5rem;
-    width: 100%;
-    overflow: auto;
-    justify-content: center;
-    height: auto;
+.wrap_card {
+  display: flex;
+  margin-top: 0.5rem;
+  width: 100%;
+  overflow: auto;
+  justify-content: center;
+  height: auto;
 }
 </style>
