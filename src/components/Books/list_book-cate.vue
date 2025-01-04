17d76bc6942112e1737e618b18725e778ecd07cd
Vipha-Rem <sokvipha.rem@gmail.com>
2025-01-04 16:05:48 +0700

    try to push any changes to origin
    can be ignored

---
+++ src/components/Books/list_book-cate.vue
@@ -0,0 +1,33 @@
+<script >
+
+export default {
+    name: "list_book_cate",
+    
+}
+
+</script>
+
+<template>
+    <div>
+        <div>
+            <h2>Search by Genre</h2>
+            <input type="checkbox" name="opt_1" id="">
+            <label for="opt_1">Science-fiction</label>
+            <input type="checkbox" name="opt_2" id="">
+            <label for="opt_2">Fantasty</label>
+            <input type="checkbox" name="opt_3" id="">
+            <label for="opt_3">Romance</label>
+            <input type="checkbox" name="opt_4" id="">
+            <label for="opt_4">Motivation</label>
+            <input type="checkbox" name="opt_4" id="">
+            <label for="opt_4">Mystery</label>
+        </div>
+        <div>
+            <h2>Search by Year</h2>
+            <input type="number" name="number_opt" id="">
+        </div>
+        <div>
+            
+        </div>
+    </div>
+</template>
--- src/views/List_book.vue
+++ src/views/List_book.vue
@@ -1,11 +1,15 @@
 <script>
-
+import list_book_cate from '../components/Books/list_book-cate.vue'
 export default{
-    name : "List_book"
+    name : "List_book",
+    components:{
+        list_book_cate,
+    }
 }
 
 </script>
 
 <template>
     <h2>List book page</h2>
+    <list_book_cate />
 </template>
