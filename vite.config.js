175cd8a99a03329f2419635147dbddbd293337fd
Mengleang30 <novmengleang14@gmail.com>
2025-06-14 15:00:01 +0700

    fix direct problem6

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,4 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
@@ -15,4 +14,9 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  preview: {
+    host: true, // listen on all addresses (0.0.0.0)
+    port: process.env.PORT ? Number(process.env.PORT) : 4173, // Railway's port or default
+    allowedHosts: ['e-commerce-book-store.up.railway.app'] // your Railway hostname
+  }
 })
